9240cdef169380e01a2451b26094a7329f3a0bfd
Andrew Walker <hovercar00@gmail.com>
2018-01-14 00:31:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0);
-  return array;
+  return array.slice(1);
 }
+
+function 
