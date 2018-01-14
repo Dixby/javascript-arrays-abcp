3f50f2707b76f20280c4e8232db5cef80c955626
Andrew Walker <hovercar00@gmail.com>
2018-01-14 00:18:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(chocolateBars, "baby ruth") {
-  return chocolateBars.push("baby ruth");
+function addElementToBeginningOfArray(array, element) {
+  return array.push(element);
 }
+
